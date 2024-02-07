fc8a244d3991445528bdfc2f533249d12469a65d
paulazzo8711 <70339585+paulazzo8711@users.noreply.github.com>
2024-02-07 19:21:37 +0100

    Predicting cognitive load and displaying alert from local file

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,54 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import axios from "axios";
+import FormData from "form-data";
+import * as fs from "fs";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log("Your Cognitive load will be estimated every 3 minutes.");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cognitiveloadestimator" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "cognitiveloadestimator.uploadFile",
+    async () => {
+      uploadFileAndShowResult();
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cognitiveloadestimator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CognitiveLoadEstimator!');
-	});
+  context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
+  //uploadFileAndShowResult();
+
+  const intervalId = setInterval(() => {
+    uploadFileAndShowResult();
+  }, 180000);
+
+  context.subscriptions.push(
+    new vscode.Disposable(() => clearInterval(intervalId))
+  );
 }
 
-// This method is called when your extension is deactivated
+async function uploadFileAndShowResult() {
+  try {
+    const filePath = "D://Desktop//UNI//Thesis//Thesis models//ID028_ET_1.xlsx";
+    const formData = new FormData();
+    formData.append("file", fs.createReadStream(filePath));
+
+    const response = await axios.post(
+      "https://fyp-paul-7dfcd301ca89.herokuapp.com/predict",
+      formData,
+      {
+        headers: {
+          ...formData.getHeaders(),
+        },
+      }
+    );
+
+    vscode.window.showInformationMessage(
+      `Mean Predicted Cognitive Load: ${response.data.mean_predicted_cognitive_load}`
+    );
+  } catch (error) {
+    vscode.window.showErrorMessage("Failed to upload file and get response.");
+    console.error(error);
+  }
+}
+
 export function deactivate() {}
