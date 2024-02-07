6c329afff32a2a82e205f330054f2a4f6c8eef9c
paulazzo8711 <70339585+paulazzo8711@users.noreply.github.com>
2024-02-07 19:56:44 +0100

    Showing live updates in status bar

--- src/extension.ts
+++ src/extension.ts
@@ -3,34 +3,71 @@ import axios from "axios";
 import FormData from "form-data";
 import * as fs from "fs";
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log("Your Cognitive load will be estimated every 3 minutes.");
+const path = require("path");
+let statusBarBtn: vscode.StatusBarItem;
+let lastCognitiveLoad: number | null = null;
+let isEstimating = false;
 
-  let disposable = vscode.commands.registerCommand(
-    "cognitiveloadestimator.uploadFile",
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Your extension "cognitiveloadestimator" is now active!');
+
+  statusBarBtn = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
+  statusBarBtn.command = "cognitiveloadestimator.startEstimation";
+  statusBarBtn.text = "$(cloud-upload) Begin Estimating Cognitive Load";
+  statusBarBtn.tooltip = "Begin Estimating Cognitive Load";
+  statusBarBtn.show();
+
+  let startEstimationDisposable = vscode.commands.registerCommand(
+    "cognitiveloadestimator.startEstimation",
     async () => {
-      uploadFileAndShowResult();
+      if (!isEstimating) {
+        isEstimating = true;
+        statusBarBtn.text = "$(sync~spin) Estimating...";
+        await uploadFileAndShowResult();
+
+        const intervalId = setInterval(async () => {
+          await uploadFileAndShowResult();
+        }, 1800000);
+
+        context.subscriptions.push(
+          new vscode.Disposable(() => clearInterval(intervalId))
+        );
+      }
     }
   );
 
-  context.subscriptions.push(disposable);
-
-  //uploadFileAndShowResult();
-
-  const intervalId = setInterval(() => {
-    uploadFileAndShowResult();
-  }, 180000);
-
-  context.subscriptions.push(
-    new vscode.Disposable(() => clearInterval(intervalId))
-  );
+  context.subscriptions.push(startEstimationDisposable);
 }
-
 async function uploadFileAndShowResult() {
   try {
-    const filePath = "D://Desktop//UNI//Thesis//Thesis models//ID028_ET_1.xlsx";
+    const directoryPath = "D://Desktop//UNI//Thesis//Thesis models";
+
+    const files = fs.readdirSync(directoryPath);
+
+    const excelFiles = files.filter(
+      (file) => file.endsWith(".xlsx") || file.endsWith(".xls")
+    );
+    let mostRecentFile = "";
+    let lastModified = 0;
+
+    excelFiles.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+      const stats = fs.statSync(filePath);
+      if (stats.mtimeMs > lastModified) {
+        lastModified = stats.mtimeMs;
+        mostRecentFile = filePath;
+      }
+    });
+
+    if (!mostRecentFile) {
+      vscode.window.showErrorMessage("No Excel files found.");
+      return;
+    }
+
     const formData = new FormData();
-    formData.append("file", fs.createReadStream(filePath));
+    formData.append("file", fs.createReadStream(mostRecentFile));
 
     const response = await axios.post(
       "https://fyp-paul-7dfcd301ca89.herokuapp.com/predict",
@@ -42,6 +79,17 @@ async function uploadFileAndShowResult() {
       }
     );
 
+    const currentCognitiveLoad = response.data.mean_predicted_cognitive_load;
+    let trendSymbol =
+      lastCognitiveLoad !== null
+        ? currentCognitiveLoad === lastCognitiveLoad
+          ? "$(dash)"
+          : currentCognitiveLoad > lastCognitiveLoad
+          ? "$(arrow-up)"
+          : "$(arrow-down)"
+        : "";
+    statusBarBtn.text = `$(graph) Cognitive Load: ${currentCognitiveLoad} ${trendSymbol}`;
+    lastCognitiveLoad = currentCognitiveLoad;
     vscode.window.showInformationMessage(
       `Mean Predicted Cognitive Load: ${response.data.mean_predicted_cognitive_load}`
     );
